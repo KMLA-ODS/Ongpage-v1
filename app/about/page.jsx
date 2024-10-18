54a4d7d815d512b238f60fe3ecbfc394fda0a124
developon-aaron <developon.aaron@gmail.com>
2024-10-18 11:22:03 -0400

    style: Update club history
    change flex style in desktop screen

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -109,23 +109,23 @@ const About = () => {
                     </dl>
                     <div class="circle-after"></div>
                   </li>
-                  <li className="relative flex flex-row-reverse mt-[25px] xl:mt-[40px]">
+                  <li className="relative flex flex-row-reverse xl:flex-row mt-[25px] xl:mt-[40px]">
                     <div class="circle-before"></div>
 
-                    <dl className="relative w-[100%] lg:w-[50%] pl-[44px] lg:pl-[70px]">
-                      <dt className="text-[30px] xl:text-[40px] font-bold text-primary">
+                    <dl className="relative w-[100%] lg:w-[50%] pl-[44px] lg:pl-[70px] xl:pl-0 xl: pr-[70px]">
+                      <dt className="text-[30px] xl:text-[40px] font-bold text-primary xl:text-right">
                         2023
                       </dt>
                       <dd>
                         <ul className="mt-[20px] ">
-                          <li className="relative pl-[30px]">
-                            <span className="absolute top-0 left-0 font-bold">
+                          <li className="relative pl-[30px] xl:pl-0 xl:pr-[30px] xl:text-right">
+                            <span className="absolute top-0 left-0 xl:left-auto xl:right-0 font-bold">
                               8
                             </span>
                             제 7회 라오스 해외봉사
                           </li>
-                          <li className="relative pl-[30px]">
-                            <span className="absolute top-0 left-0 font-bold">
+                          <li className="relative pl-[30px] xl:pl-0 xl:pr-[30px] xl:text-right">
+                            <span className="absolute top-0 left-0 font-bold xl:left-auto xl:right-0">
                               4
                             </span>
                             옹달샘 해외봉사 재개
@@ -152,7 +152,7 @@ const About = () => {
                           </li>
                           <li className="relative pl-[30px]">
                             <span className="absolute top-0 left-0 font-bold">
-                              12
+                              3
                             </span>
                             APA 선정 올해의 청소년 필란트로피스트{" "}
                           </li>
@@ -161,26 +161,25 @@ const About = () => {
                     </dl>
                     <div class="circle-after"></div>
                   </li>
-                  <li className="relative flex flex-row-reverse mt-[25px] xl:mt-[40px]">
+                  <li className="relative flex flex-row-reverse xl:flex-row mt-[25px] xl:mt-[40px]">
                     <div class="circle-before"></div>
-
-                    <dl className="relative w-[100%] lg:w-[50%] pl-[44px] lg:pl-[70px]">
-                      <dt className="text-[30px] xl:text-[40px] font-bold text-primary">
+                    <dl className="relative w-[100%] lg:w-[50%] pl-[44px] lg:pl-[70px] xl:pl-0 xl: pr-[70px]">
+                      <dt className="text-[30px] xl:text-[40px] font-bold text-primary xl:text-right">
                         2014
                       </dt>
                       <dd>
-                        <ul className="mt-[20px]">
-                          <li className="relative pl-[30px]">
-                            <span className="absolute top-0 left-0 font-bold">
-                              2
-                            </span>
-                            옹달샘 설립
-                          </li>
-                          <li className="relative pl-[30px]">
-                            <span className="absolute top-0 left-0 font-bold">
+                        <ul className="mt-[20px] ">
+                          <li className="relative pl-[30px] xl:pl-0 xl:pr-[30px] xl:text-right">
+                            <span className="absolute top-0 left-0 xl:left-auto xl:right-0 font-bold">
                               8
                             </span>
                             제 1회 베트남 해외봉사
+                          </li>
+                          <li className="relative pl-[30px] xl:pl-0 xl:pr-[30px] xl:text-right">
+                            <span className="absolute top-0 left-0 font-bold xl:left-auto xl:right-0">
+                              4
+                            </span>
+                            옹달샘 설립
                           </li>
                         </ul>
                       </dd>
@@ -197,11 +196,10 @@ const About = () => {
                     라오스, 베트남, 캄보디아 등<br></br> 다양한 국가에서
                     봉사활동을 전개합니다.
                   </h2>
-                  <p className="font-light text-gray-600 text-[15px] mt-2 text-left xl:text-left xl:text-[16px] xl:font-normal">
-                    2023년, 코로나 19 팬데믹 이후 라오스 방비엥 마을에서{" "}
-                    <br></br>
-                    멤브레인 정수기 설치와 위생교육을 실시했습니다.<br></br>{" "}
-                    옹달샘은 지속 가능한 발전을 위해 꾸준한 유지보수하는중
+                  <p className="font-light text-gray-600 text-[16px] mt-2 text-left xl:text-left xl:text-[16px] xl:font-normal">
+                    2023년, 코로나 19 팬데믹 이후 라오스 방비엥에서 <br></br>
+                    멤브레인 정수기 설치와 위생교육을 재개했습니다.<br></br>{" "}
+                    옹달샘은 언제나 지속 가능한 발전을 위해 노력합니다.
                   </p>
                 </div>
                 <ScrollAreaHorizontalDemo></ScrollAreaHorizontalDemo>
