a349816b04e67327f53558ec70fe0f9a6ce1e568
developon-aaron <developon.aaron@gmail.com>
2024-10-24 13:12:51 -0400

    fix: Fix activity navbar error
    fix text separating error in activity small navbar

--- app/activity/page.jsx
+++ app/activity/page.jsx
@@ -47,45 +47,63 @@ const projects = [
 
 const Activity = () => {
   return (
-    <section className="">
+    <section className="mb-12 xl:mb-16">
       <div className="h-[295px] xl:h-[315px] bg-[url('/images/ActivityBanner.webp')] bg-cover bg-center">
         <div className="container h-full flex items-end pl-[12px] pb-[40px] lg:pl-0 lg:pb-0 lg:items-center">
           <h2 className="text-[40px] font-bold text-white">활동소개</h2>
         </div>
       </div>
-      <div className="min-h-[80vh] flex items-center justify-center relative">
-        <div className="container mx-auto">
+      <div className="flex items-center justify-center relative">
+        <div className="container mx-auto ">
           <Tabs
-            defaultValue="volunteering"
+            defaultValue="oversees-volunteering"
             className="w-full flex flex-col items-center relative"
           >
-            <TabsList className="absolute top-[-65px]">
-              <TabsTrigger value="volunteering">해외봉사</TabsTrigger>
-              <TabsTrigger value="technology">기술개발</TabsTrigger>
-              <TabsTrigger value="fundraising">모금사업</TabsTrigger>
+            <TabsList className="absolute top-[-25px]">
+              <TabsTrigger value="oversees-volunteering">해외봉사</TabsTrigger>
+              <TabsTrigger value="tech">기술개발</TabsTrigger>
+              <TabsTrigger value="fd">모금사업</TabsTrigger>
             </TabsList>
 
             {/* Contents */}
             <div className="min-h-[70vh] w-full">
-              <TabsContent value="volunteering">
-                {/* photos & description */}
-
-                <div className="mt-[40px] xl:mt-[60px] xl:mb-[40px]">
-                  <h2 className="font-bold text-[18px] text-left xl:text-left xl:text-[35px]">
-                    매년 옹달샘은 <span className="text-primary">현장으로</span>{" "}
-                    나섭니다.
+              <TabsContent value="oversees-volunteering">
+                {/* description */}
+                <div className="w-full flex flex-col items-center pt-[97px]">
+                  <Image
+                    src="/images/ddaomdown.svg"
+                    alt="ongdalsam"
+                    width={24}
+                    height={24}
+                  ></Image>
+                  <h2 className=" text-[18px] xs:text-[20px] font-bold xl:text-[30px] my-[23px] text-center">
+                    해외봉사{" "}
                   </h2>
-                  <p className="font-light text-gray-600 text-[16px] mt-2 text-left xl:text-left xl:text-[16px] xl:font-normal">
-                    2023년, 코로나 19 팬데믹 이후 라오스 방비엥에서 <br></br>
-                    멤브레인 정수기 설치와 위생교육을 재개했습니다.<br></br>{" "}
-                    옹달샘은 언제나 지속 가능한 발전을 위해 노력합니다.
+                  <Image
+                    src="/images/ddaomup.svg"
+                    alt="ongdalsam"
+                    width={24}
+                    height={24}
+                  ></Image>
+                  <p className=" text-[14.5px] xs:text-[16px] xl:text-[20px] text-center mt-[54px]">
+                    2014년 설립된 옹달샘은
+                    <br className="xl:hidden"></br> 깨끗한 식수를 필요로 하는
+                    개발도상국 아동과 마을을 <br></br> 위해 식수 공급 및
+                    위생교육 사업과 진행하는 <br className="xl:hidden"></br>
+                    <span className="font-semibold text-primary">
+                      적정기술 해외봉사 동아리
+                    </span>
+                    입니다.
                   </p>
+                  <div className="w-[35px] h-[2px] xl:w-[47px] xl:h-[3px] bg-[#D9D9D9] mt-[40px] xl:mt-[54px]"></div>
                 </div>
-
-                {/* gallery */}
               </TabsContent>
-              <TabsContent value="technology"></TabsContent>
-              <TabsContent value="fundraising"></TabsContent>
+              <TabsContent value="tech">
+                <div className="mt-[50px]">technology</div>
+              </TabsContent>
+              <TabsContent value="fd">
+                <div className="mt-[50px]">fd</div>
+              </TabsContent>
             </div>
           </Tabs>
         </div>
