6edc08fd7d8edfb6e6022a52fed665c01ab6c152
developon-aaron <developon.aaron@gmail.com>
2024-10-30 11:31:17 -0400

    style: Add meta image
    add meta image

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,6 +21,21 @@ export const metadata = {
   keywords: "적정기술, 봉사, 민족사관고등학교, 해외봉사, 옹달샘",
   author: "Ongdalsam",
   robots: "index",
+  openGraph: {
+    title: "민족사관고등학교 옹달샘",
+    description:
+      "민족사관고등학교 재학생 및 졸업생으로 이루어진 적정기술 해외봉사 동아리입니다.",
+    images: [
+      {
+        url: "/images/Ongdalsam.png",
+        width: 1200,
+        height: 630,
+        alt: "민족사관고등학교 옹달샘 대표 이미지",
+      },
+    ],
+    type: "website",
+    locale: "ko_KR",
+  },
 };
 
 export default function RootLayout({ children }) {
